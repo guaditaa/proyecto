f05c9341b77fdbc5af4bce98adceac690d19f447
carlo-coding <jaanc.rt@gmail.com>
2022-10-03 13:50:08 -0500

    Removed config values

--- java_general/user_session/config.js
+++ java_general/user_session/config.js
@@ -1,5 +1,5 @@
 export default {
-  PARSE_APP_ID: "r2nNLO2cTjTWs0TCCXxUpYUYOl6edNkgbOg4uNQh",
-  PARSE_JS_KEY: "ObKSROpvs3f66Ly4H5EyqvXAtu9B1E7oDVqaxKOh",
+  PARSE_APP_ID: "",
+  PARSE_JS_KEY: "",
   PARSE_SERVER_URL: "https://parseapi.back4app.com/"
 }
